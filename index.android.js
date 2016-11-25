8811f6d003de9394d8f97a38685725f4032df48f
Luis Alberto Cruz <luicruz.amaya@gmail.com>
2016-11-24 18:31:30 -0600

    Some changes

--- index.android.js
+++ index.android.js
@@ -1,53 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Image } from 'react-native';
 
-export default class AwesomeProject extends Component {
+class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Image source={this.state.test} style={{width: 193, height: 110}}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+AppRegistry.registerComponent('Bananas', () => Bananas);
 
+componentDidMount() {
+    this.setState({
+        test: JSON.parse(document.getElementById('json-data').innerHTML)
+    });
+}
+
+var myJson = (
+  <script type="text/json" id="json-data">
+      {'path': 'image_path', 'name':'image_name'} // el path debe ser desde el directorio donde se ejecuta esto, por eljemplo si estamos en el directorio src pat = "../image/image.jpg"
+  </script>
+);
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+
